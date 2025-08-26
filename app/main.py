ba46a40bac5d7ccd0cedfb2dd8b408748145c8c3
Gena <gena.anzin@ukr.net>
2025-08-26 10:29:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ class BaseRobot:
         self.weight = weight
         self.coords = [0, 0]
 
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
     def go_forward(self, step: int=1) -> None:
         self.coords[1] += step
 
@@ -33,8 +36,16 @@ class FlyingRobot(BaseRobot):
         self.coords[2] -= step
 
 
-class DeliveryRobot(FlyingRobot):
+class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str, weight: int, max_load_weight: int, current_load: Cargo=None ) -> None:
         super().__init__(name= name, weight=weight)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None:
+            if cargo.weight <= self.max_load_weight:
+                self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
